710e3cb7f9461c7a24c8533579459028ef5741a4
Aditya Kinjawadekar <kinjawadekaradi112@gmail.com>
2024-11-19 13:53:40 +0530

    feat: orderbook

--- 2_orderbook-engine/main.go
+++ 2_orderbook-engine/main.go
@@ -1,8 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"engine/orderbook"
+	"container/heap"
 )
 
 
-func main(){}
+func main(){
+	// bids := &MaxHeap{}
+	// asks := &MinHeap{}
+	// heap.Init(bids)
+	// heap.Init(asks)
+
+	fmt.Println("engine started")
+	ob := orderbook.Orderbook{
+		Market : "SOL/USDC",
+		Asks : &orderbook.MinHeap{},
+		Bids : &orderbook.MaxHeap{},
+		CurrentPrice : 0,
+	}
+
+
+	heap.Push(ob.Bids, &orderbook.OrderEntry{Price: 100.5, Quantity: 10})
+	heap.Push(ob.Bids, &orderbook.OrderEntry{Price: 101.0, Quantity: 5})
+	heap.Push(ob.Bids, &orderbook.OrderEntry{Price: 99.0, Quantity: 20})
+
+	heap.Push(ob.Asks, &orderbook.OrderEntry{Price: 98.5, Quantity: 15})
+	heap.Push(ob.Asks, &orderbook.OrderEntry{Price: 97.0, Quantity: 8})
+
+	
+}
--- 2_orderbook-engine/orderbook/orderbook.go
+++ 2_orderbook-engine/orderbook/orderbook.go
@@ -1,33 +1,70 @@
 package orderbook
 
-
-
-
+// some basic initialisations for orderbook
 type Order struct {
-	Kind     string  
-	Market 	string
-	Price    float64 
-	Quantity int     
+	Kind     string
+	Market   string
+	Price    float64
+	Quantity int
 }
-type MaxHeap []*Order
-func (h MaxHeap) Len() int            { return len(h) }
-func (h MaxHeap) Less(i, j int) bool  { return h[i].Price > h[j].Price } 
-// func (h MaxHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(*Order)) }
-func (h *MaxHeap) Pop() interface{}   { old := *h; n := len(old); x := old[n-1]; *h = old[0 : n-1]; return x }
 
+type OrderEntry struct {
+	Price    float64
+	Quantity float64
+}
 
-type MinHeap []*Order
+type MaxHeap []*OrderEntry
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i].Price > h[j].Price }
+
+func (h MaxHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(*OrderEntry)) }
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+type MinHeap []*OrderEntry
+
 func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i].Price < h[j].Price } 
-// func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(*Order)) }
-func (h *MinHeap) Pop() interface{}  { old := *h; n := len(old); x := old[n-1]; *h = old[0 : n-1]; return x }
+func (h MinHeap) Less(i, j int) bool { return h[i].Price < h[j].Price }
 
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(*OrderEntry)) }
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
-type OrderBook struct {
-	asks *MinHeap 
-	bids *MaxHeap 
-	currentPrice float64
-	market string
+// orderbook
+type Orderbook struct {
+	Market       string
+	Asks         *MinHeap
+	Bids         *MaxHeap
+	CurrentPrice float64
+}
+
+func (ob *Orderbook) Buy(price float64, quantity float64) {
+
+}
+
+func (ob *Orderbook) Sell(price float64, quantity float64) {
+
+}
+
+func (ob *Orderbook) GetDepth() ([]*OrderEntry, []*OrderEntry){
+	bids := make([]*OrderEntry, len(*ob.Bids))
+	copy(bids, *ob.Bids)
+
+	asks := make([]*OrderEntry, len(*ob.Asks))
+	copy(asks, *ob.Asks)
+
+	return  bids, asks
 }
